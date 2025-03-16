5751feefd9eb20ff5738b51fc413289303b62a78
Josh Bilsland <85071575+JoshuaBilsland@users.noreply.github.com>
2025-03-16 10:27:46 +0000

    feat: add main loop

--- main.py
+++ main.py
@@ -76,5 +76,26 @@ def add():
     desciption = get_description()
     CSV.add_entry(date, amount, category, desciption)
 
-CSV.get_transactions("01/01/2021", "16/03/2025")
-add()
+
+def main():
+    while True:
+        print("\n1. Add a transaction")
+        print("2. View transactions withing a date range")
+        print("3. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            add()
+        elif choice == "2":
+            start_date = get_date("Enter the start date (dd/mm/yyyy): ")
+            end_date = get_date("Enter the end date (dd/mm/yyyy): ")
+            df = CSV.get_transactions(start_date, end_date)
+        elif choice == "3":
+            print("Exiting...")
+            break
+        else:
+            print("Invalid choice! Try again.")
+
+
+if __name__ == "__main__":
+    main()
